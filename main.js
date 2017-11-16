a35866bd0fc3d080707e10c66bca4fb09f291287
Maksim <maksim.ramanenkau@gmail.com>
2017-11-16 16:32:44 +0300

    canvas size update

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 'use strict'
-
 let polygonPointsA = [[100,100], [200,100], [200,200], [100,200]];
 let polygonPointsB = [[300,400], [450,300], [500,300], [470,350]];
 
@@ -8,12 +7,14 @@ let fillColor = '#f00';
 let poly1 = new Polygon(polygonPointsA, strokeColor, fillColor);
 let poly2 = new Polygon(polygonPointsB, strokeColor, fillColor);
 
+
 let setting = {
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: window.innerWidth - window.innerWidth * 0.02,
+    height: window.innerHeight - window.innerWidth * 0.02,
     padding: 30,
     polygonMargin: 20
 }
+
 let canvas = new Canvas('example', setting);
 
 window.addEventListener('load', canvas.init());
